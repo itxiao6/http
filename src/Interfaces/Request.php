3b82bfc96a82d87b5ddfd58ee206fedea5d491e6
liuguangcai <itxiao6@qq.com>
2017-11-17 22:09:19 +0800

    请求和请求的接口类

--- src/Interfaces/Request.php
+++ src/Interfaces/Request.php
@@ -7,5 +7,46 @@ namespace Itxiao6\Http\Interfaces;
  */
 interface Request
 {
+    /**
+     * 获取session实例
+     * @return mixed
+     */
+    public static function session();
+
+    /**
+     * 获取cookie实例
+     * @return mixed
+     */
+    public static function cookie();
+
+    /**
+     * 设置请求uri
+     * @return mixed
+     */
+    public function set_rquest_uri();
+
+    /**
+     * 设置请求url
+     * @return mixed
+     */
+    public function set_rquest_url();
+
+    /**
+     * 设置post数据
+     * @return mixed
+     */
+    public function set_post_data();
+
+    /**
+     * 设置get数据
+     * @return mixed
+     */
+    public function set_get_data();
+
+    /**
+     * 设置文件上传数据
+     * @return mixed
+     */
+    public function set_file_data();
 
 }
