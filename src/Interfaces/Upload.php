93151cf15c5912a2175063db50de958cbd60011e
liuguangcai <itxiao6@qq.com>
2017-11-17 22:14:40 +0800

    文件上传工具类 和 接口

---
+++ src/Interfaces/Upload.php
@@ -0,0 +1,11 @@
+<?php
+namespace Itxiao6\Http\Interfaces;
+/**
+ * 文件上传工具类 接口
+ * Interface Upload
+ * @package Itxiao6\Http\Interfaces
+ */
+interface Upload
+{
+
+}
---
+++ src/Tools/Upload.php
@@ -0,0 +1,12 @@
+<?php
+namespace Itxiao6\Http\Tools;
+
+/**
+ * 文件上传工具类
+ * Class Upload
+ * @package Itxiao6\Http\Tools
+ */
+class Upload implements \Itxiao6\Http\Interfaces\Upload
+{
+
+}
